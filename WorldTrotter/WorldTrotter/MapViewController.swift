a7a43f07eb0b76fd8cb4418f1136bcb83784e199
Matt Green <Matt.Green@mead354.org>
2018-03-22 08:58:18 -0700

    chapter 5 complete

---
+++ WorldTrotter/WorldTrotter/MapViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by Matt Green on 3/22/18.
+//  Copyright © 2018 Matt Green. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("MapViewController loaded its view ------")
+    }
+
+}
