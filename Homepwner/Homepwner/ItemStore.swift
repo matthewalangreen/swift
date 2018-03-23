a4159f44477bb6eb5e373cb76854c8a1080dcfa5
Matt Green <Matt.Green@mead354.org>
2018-03-23 15:47:40 -0700

    p 192

---
+++ Homepwner/Homepwner/ItemStore.swift
@@ -0,0 +1,33 @@
+//
+//  ItemStore.swift
+//  Homepwner
+//
+//  Created by Matt Green on 3/23/18.
+//  Copyright © 2018 Matt Green. All rights reserved.
+//
+
+import UIKit
+
+class ItemStore {
+    
+    var allItems = [Item]()
+    
+    @discardableResult func createItem() -> Item {
+        let newItem = Item(random: true)
+        
+        allItems.append(newItem)
+        
+        return newItem
+    }
+    
+    init() {
+        for _ in 0..<5 {
+            createItem()
+        }
+    }
+
+}
+
+
+
+
