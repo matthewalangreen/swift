6c3ec237067dae808906daddd61d6d6b721886e9
Matt Green <Matt.Green@mead354.org>
2018-03-24 21:22:25 -0700

    p 238

--- Homepwner/Homepwner/ItemsViewController.swift
+++ Homepwner/Homepwner/ItemsViewController.swift
@@ -106,6 +106,23 @@ class ItemsViewController: UITableViewController {
         // updagte the model
         itemStore.moveItem(from: sourceIndexPath.row, to: destinationIndexPath.row)
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // if the triggerd segue is the "showItem" segue
+        switch segue.identifier {
+        case "showItem"?:
+            // figure out which row was just tapped
+            if let row = tableView.indexPathForSelectedRow?.row {
+                
+                // Get the item associated  with this row and pass it along
+                let item = itemStore.allItems[row]
+                let detailViewController = segue.destination as! DetailViewController
+                detailViewController.item = item
+            }
+        default:
+            preconditionFailure("Unexpected segue identifier.")
+        }
+    }
 
 }
 
