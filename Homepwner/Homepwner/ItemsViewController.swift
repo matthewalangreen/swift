9dc843752608a2498eb0a10da102c50eca032da7
Matt Green <Matt.Green@mead354.org>
2018-03-25 09:50:19 -0700

    p246

--- Homepwner/Homepwner/ItemsViewController.swift
+++ Homepwner/Homepwner/ItemsViewController.swift
@@ -123,7 +123,12 @@ class ItemsViewController: UITableViewController {
             preconditionFailure("Unexpected segue identifier.")
         }
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        tableView.reloadData()
+    }
 }
 
 
