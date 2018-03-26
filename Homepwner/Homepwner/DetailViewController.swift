f0d0bb0659288370c2e58ce2f08e07f81abfb608
Matt Green <Matt.Green@mead354.org>
2018-03-26 13:49:05 -0700

    p268

--- Homepwner/Homepwner/DetailViewController.swift
+++ Homepwner/Homepwner/DetailViewController.swift
@@ -8,12 +8,30 @@
 
 import UIKit
 
-class DetailViewController: UIViewController, UITextFieldDelegate{
+class DetailViewController: UIViewController, UITextFieldDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate{
+    
     @IBOutlet var imageView: UIImageView! // different order than book
     @IBOutlet var nameField: UITextField!
     @IBOutlet var serialNumberField: UITextField!
     @IBOutlet var valueField: UITextField!
     @IBOutlet var dateLabel: UILabel!
+    
+    @IBAction func takePicture(_ sender: UIBarButtonItem) {
+        let imagePicker = UIImagePickerController()
+        
+        // if the device has a camera, take a picture, otherwise,
+        // just pick a photo from the library
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            imagePicker.sourceType = .camera
+        } else {
+            imagePicker.sourceType = .photoLibrary
+        }
+        
+        imagePicker.delegate = self
+        
+        // place an image on screen
+        present(imagePicker, animated: true, completion: nil)
+    }
     
     @IBAction func backgroundTapped(_ sender: UITapGestureRecognizer) {
         view.endEditing(true)
