a159a3f1277b4592dc516986a897d433009334a7
Matt Green <Matt.Green@mead354.org>
2018-03-29 18:05:05 -0700

    p 359

---
+++ Photorama/Photorama/PhotoStore.swift
@@ -0,0 +1,37 @@
+//
+//  PhotoStore.swift
+//  Photorama
+//
+//  Created by Matt Green on 3/29/18.
+//  Copyright © 2018 Matt Green. All rights reserved.
+//
+
+import Foundation
+
+class PhotoStore {
+    
+    private let session: URLSession = {
+        let config = URLSessionConfiguration.default
+        return URLSession(configuration: config)
+    }()
+    
+    func fetchInterestingPhotos() {
+        
+        let url  = FlickrAPI.interestingPhotosURL
+        let request = URLRequest(url: url)
+        let task = session.dataTask(with: request) {
+            (data, response, error) -> Void in
+            
+            if let jsonData = data {
+                if let jsonString = String(data: jsonData, encoding: .utf8) {
+                    print(jsonString)
+                }
+            } else if let requestError = error {
+                print("error fetching interesting photos: \(requestError)")
+            } else {
+                print("unexpected error with the request")
+            }
+        }
+        task.resume()
+    }
+}
--- Photorama/Photorama/PhotosViewController.swift
+++ Photorama/Photorama/PhotosViewController.swift
@@ -10,5 +10,12 @@ import UIKit
 
 class PhotosViewController: UIViewController {
     @IBOutlet var imageView: UIImageView!
+    var store: PhotoStore!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        store.fetchInterestingPhotos()
+    }
     
 }
