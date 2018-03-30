1215a34330269bda01edfa14011e6e3d4b0ac607
Matt Green <Matt.Green@mead354.org>
2018-03-30 15:24:45 -0700

    p 391

---
+++ Photorama/Photorama/PhotoCollectionViewCell.swift
@@ -0,0 +1,48 @@
+//
+//  PhotoCollectionViewCell.swift
+//  Photorama
+//
+//  Created by Matt Green on 3/30/18.
+//  Copyright © 2018 Matt Green. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var spinner: UIActivityIndicatorView!
+    
+    func update(with image: UIImage?) {
+        if let imageToDisplay = image {
+            spinner.stopAnimating()
+            imageView.image = imageToDisplay
+        } else {
+            spinner.startAnimating()
+            imageView.image = nil
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        update(with: nil)
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        update(with: nil)
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
