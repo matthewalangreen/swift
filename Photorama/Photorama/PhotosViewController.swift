9df341f3da3e107a86eb418ff4c92d3fea036e10
Matt Green <Matt.Green@mead354.org>
2018-03-29 17:35:41 -0700

    p 351

---
+++ Photorama/Photorama/PhotosViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PhotosViewController.swift
+//  Photorama
+//
+//  Created by Matt Green on 3/29/18.
+//  Copyright © 2018 Matt Green. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    
+}
