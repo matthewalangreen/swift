d30c1096d1ce7a572697b5f9f0bce5e72fdaca92
Matt Green <Matt.Green@mead354.org>
2018-04-05 17:20:14 -0700

    p 426

--- Photorama/Photorama/TagsViewController.swift
+++ Photorama/Photorama/TagsViewController.swift
@@ -21,6 +21,7 @@ class TagsViewController: UITableViewController {
         super.viewDidLoad()
         
         tableView.dataSource = tagDataSource
+        tableView.delegate = self
     
         updateTags()
     }
@@ -32,10 +33,49 @@ class TagsViewController: UITableViewController {
         switch tagsResult {
         case let .success(tags):
             self.tagDataSource.tags = tags
+            
+            guard let photoTags = self.photo.tags as? Set<Tag> else {
+                return
+            }
+            
+            for tag in photoTags {
+                if let index = self.tagDataSource.tags.index(of: tag) {
+                    let indexPath = IndexPath (row: index, section: 0)
+                    self.selectedIndexPaths.append(indexPath)
+                }
+            }
         case let .failure(error):
             print("Error fetching tags: \(error).")
         }
         self.tableView.reloadSections(IndexSet(integer: 0), with: .automatic)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let tag =  tagDataSource.tags[indexPath.row]
+        
+        if let index = selectedIndexPaths.index(of: indexPath) {
+            selectedIndexPaths.remove(at: index)
+            photo.removeFromTags(tag)
+        } else {
+            selectedIndexPaths.append(indexPath)
+            photo.addToTags(tag)
+        }
+        
+        do {
+            try store.persistentContainer.viewContext.save()
+        } catch {
+            print("Core data save failed \(error)")
+        }
+        
+        tableView.reloadRows(at: [indexPath], with: .automatic)
+    }
+    
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if selectedIndexPaths.index(of: indexPath) != nil {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
         }
     }
 }
