b504c96f8d7dece6a204919375d6ac5231025604
Matt Green <Matt.Green@mead354.org>
2018-04-06 10:59:22 -0700

    Book compelte

--- Photorama/Photorama/PhotoCollectionViewCell.swift
+++ Photorama/Photorama/PhotoCollectionViewCell.swift
@@ -13,6 +13,35 @@ class PhotoCollectionViewCell: UICollectionViewCell {
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var spinner: UIActivityIndicatorView!
     
+    var photoDescription: String?
+    
+    override var accessibilityLabel: String? {
+        get {
+            return photoDescription
+        }
+        set {
+            // igonre attempts to set
+        }
+    }
+    
+    override var  isAccessibilityElement: Bool {
+        get {
+            return true
+        }
+        set {
+            super.isAccessibilityElement = newValue
+        }
+    }
+    
+    override var accessibilityTraits: UIAccessibilityTraits {
+        get {
+            return super.accessibilityTraits | UIAccessibilityTraitImage
+        }
+        set {
+            // ignore attempts to set
+        }
+    }
+    
     func update(with image: UIImage?) {
         if let imageToDisplay = image {
             spinner.stopAnimating()
