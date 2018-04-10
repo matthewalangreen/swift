06216294040a7d26b22f61b79672cc5c54476a00
Matt Green <Matt.Green@mead354.org>
2018-04-10 16:46:09 -0700

    Lesson 17.3 done

---
+++ Introduction to Swift Development - Apple Course/ColorMix/ColorMix/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  ColorMix
+//
+//  Created by Matt Green on 4/10/18.
+//  Copyright © 2018 Matt Green. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redSwitch: UISwitch!
+    @IBOutlet var greenSwitch: UISwitch!
+    @IBOutlet var blueSwitch: UISwitch!
+    
+    @IBAction func switchChanged(_ sender: UISwitch) {
+        updateColor()
+    }
+    
+    func updateColor() {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        if redSwitch.isOn {
+            red = 1
+        }
+        if greenSwitch.isOn {
+            green = 1
+        }
+        if blueSwitch.isOn {
+            blue = 1
+        }
+        
+        let color = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        colorView.backgroundColor = color
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        colorView.backgroundColor = .black
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+}
+
