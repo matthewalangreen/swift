e3d4b5b753d37b0e0885e2453e8eb0ddc69f74d2
Matt Green <Matt.Green@mead354.org>
2018-08-10 09:11:09 -0700

    Working to implement a check to get today's data (not overwrite -- which is current behavior)

--- ChoiceTracker/ChoiceTracker/DailyRecordStore.swift
+++ ChoiceTracker/ChoiceTracker/DailyRecordStore.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class DailyRecordStore {
-    var allDailyRecords = [DailyRecord]()
+    //MARK:- Properties
+    var allDailyRecords: Dictionary = [String:DailyRecord]()
     
     let recordsArchiveURL: URL = {
         let documentsDirectories = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
@@ -17,20 +18,47 @@ class DailyRecordStore {
         return documentDirectory.appendingPathComponent("dailyRecords.archive")
     }()
     
-    @discardableResult func createDailyRecord() -> DailyRecord {
-        let newDailyRecord = DailyRecord()
-        
-        allDailyRecords.append(newDailyRecord)
-        
-        return newDailyRecord
-    }
-    
+    //MARK:- Initializer
     init() {
-        if let archivedItems = NSKeyedUnarchiver.unarchiveObject(withFile: recordsArchiveURL.path) as? [DailyRecord] {
+        if let archivedItems = NSKeyedUnarchiver.unarchiveObject(withFile: recordsArchiveURL.path) as? [String:DailyRecord] {
             allDailyRecords = archivedItems
         }
     }
     
+    //Mark:- Methods
+    
+    @discardableResult func createDailyRecord() -> DailyRecord {
+        let newDailyRecord = DailyRecord()
+        
+        // make dateFormatter a reusable closure
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .none
+        dateFormatter.locale = Locale(identifier: "en_US")
+        // end dateFormatter
+        
+        let nowDateString = dateFormatter.string(from: Date.init())
+        
+        allDailyRecords[nowDateString] = newDailyRecord
+        
+        return newDailyRecord
+    }
+    
+    func doesDateExistQ(date: String) -> Bool {
+        return allDailyRecords.keys.contains(date)
+    }
+    
+    //MARK:-Start here -- work to implement this function so we can always get the correct day's data
+    func getCurrentDayRecord() -> DailyRecord {
+        // check allDailyRecords for today, if today exists return
+        
+        // if today doesn't exist, make it and return it.
+        return createDailyRecord()
+    }
+    
+  
+    
+    //MARK:- NSCoding Methods
     func saveChanges() -> Bool {
         print("Saving items to: \(recordsArchiveURL.path)")
         return NSKeyedArchiver.archiveRootObject(allDailyRecords, toFile: recordsArchiveURL.path)
